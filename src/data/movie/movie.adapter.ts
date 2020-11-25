7c414e51fffbaabfbcfb3556f843f457ad48836c
Muhammad Naufal <muhammad_naufal@hotmail.com>
2020-11-25 19:23:19 +0700

    add movie service and adapter

---
+++ src/data/movie/movie.adapter.ts
@@ -0,0 +1,34 @@
+import { Movie, MovieList, Cast, MovieDetail } from 'types/Movie';
+
+export function movieAdapter(movie: any): Movie {
+  return {
+    id: movie.id,
+    title: movie.title,
+    rating: movie.vote_average,
+    overview: movie.overview,
+    release_date: movie.release_date,
+  };
+}
+
+export function movieListAdapter(data: any): MovieList {
+  return {
+    page: data.page,
+    results: data.results.map((movie: any) => movieAdapter(movie)),
+  };
+}
+
+export function castAdapter(cast: any): Cast {
+  return {
+    id: cast.id,
+    name: cast.name,
+    profile_path: cast.profile_path,
+  };
+}
+
+export function movieDetailAdapter(movie: any, casts: any): MovieDetail {
+  return {
+    ...movieAdapter(movie),
+    duration: movie.runtime,
+    casts: casts.map((cast: any) => castAdapter(cast)),
+  };
+}
---
+++ src/data/movie/movie.service.ts
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import { BASE_URL } from 'constant';
+import { MovieDetail, MovieList } from 'types/Movie';
+import { movieListAdapter, movieDetailAdapter } from './movie.adapter';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+interface MovieListParams {
+  page: number;
+  language?: string;
+  region?: string;
+}
+
+export async function getNowPlayingMovies({
+  page = 1,
+  language,
+  region,
+}: MovieListParams): Promise<MovieList> {
+  const params: MovieListParams = { page };
+  if (language) params.language = language;
+  if (region) params.region = region;
+
+  const URL = `${BASE_URL}/now_playing`;
+
+  const { data } = await axios.get<any>(URL, {
+    params: {
+      ...params,
+      api_key: API_KEY,
+    },
+  });
+
+  return movieListAdapter(data);
+}
+
+export async function getMovieDetail(id: number): Promise<MovieDetail> {
+  const URL_DETAIL = `${BASE_URL}/${id}`;
+  const URL_CASTS = `${BASE_URL}/${id}/credits`;
+
+  const { data: movie } = await axios.get<any>(URL_DETAIL, { params: { api_key: API_KEY } });
+  const { data: casts } = await axios.get<any>(URL_CASTS, { params: { api_key: API_KEY } });
+
+  return movieDetailAdapter(movie, casts);
+}
+
+export async function getRecommendationMovies(
+  id: number,
+  { page, language }: MovieListParams,
+): Promise<MovieList> {
+  const params: MovieListParams = { page };
+  if (language) params.language = language;
+
+  const URL = `${BASE_URL}/${id}/recommendations`;
+
+  const { data } = await axios.get<any>(URL, {
+    params: {
+      ...params,
+      api_key: API_KEY,
+    },
+  });
+
+  return movieListAdapter(data);
+}
+
+export async function getSimilarMovies(
+  id: number,
+  { page, language }: MovieListParams,
+): Promise<MovieList> {
+  const params: MovieListParams = { page };
+  if (language) params.language = language;
+
+  const URL = `${BASE_URL}/${id}/similar`;
+
+  const { data } = await axios.get<any>(URL, {
+    params: {
+      ...params,
+      api_key: API_KEY,
+    },
+  });
+
+  return movieListAdapter(data);
+}
