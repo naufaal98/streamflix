cee5b6b0ee30e7420cb6b5e28736a78a22c28f11
Muhammad Naufal <muhammad_naufal@hotmail.com>
2020-11-25 19:24:23 +0700

    Add utility for calculating price

---
+++ src/utils/calculatePriceByRating.ts
@@ -0,0 +1,15 @@
+export default function calculatePriceByRating(rating: number): number {
+  if (rating > 1 && rating <= 3) {
+    return 3500;
+  }
+  if (rating > 3 && rating <= 6) {
+    return 8250;
+  }
+  if (rating > 6 && rating <= 8) {
+    return 16350;
+  }
+  if (rating > 8 && rating <= 10) {
+    return 21250;
+  }
+  return 0;
+}
