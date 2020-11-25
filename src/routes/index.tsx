0b4a7d2e8a8bb293c011595aa836b2472288040b
Muhammad Naufal <muhammad_naufal@hotmail.com>
2020-11-26 01:18:50 +0700

    Add Home page and Detail page to route

---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from './Home';
+import Detail from './Detail';
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/:id-:slug">
+        <Detail />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  );
+};
+
+export default () => <Routes />;
