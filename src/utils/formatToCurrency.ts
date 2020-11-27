fc2e7df13b85f32f330c9b57046e3c1d6cea3575
Muhammad Naufal <muhammad_naufal@hotmail.com>
2020-11-27 12:59:42 +0700

    add formatToCurrency utility

---
+++ src/utils/formatToCurrency.ts
@@ -0,0 +1,7 @@
+export default function formatToCurrency(number: number) {
+  return new Intl.NumberFormat('id-ID', {
+    style: 'currency',
+    currency: 'IDR',
+    minimumFractionDigits: 0,
+  }).format(number);
+}
