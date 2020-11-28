b4e6d5e6ab70267aed95991bd8be171ced22218c
Muhammad Naufal <muhammad_naufal@hotmail.com>
2020-11-28 22:35:56 +0700

    Add button disabled style

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,19 +2,19 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-interface ButtonProps {
+interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
   kind?: 'primary' | 'secondary';
-  htmlType?: 'button' | 'submit' | 'reset';
+  type?: 'button' | 'submit' | 'reset';
   onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, kind, htmlType, onClick, ...other }) => {
+const Button: React.FC<ButtonProps> = ({ children, kind, type, onClick, ...other }) => {
   const buttonClass = kind === 'secondary' ? styles.Secondary : styles.Primary;
 
   return (
     <button
       // eslint-disable-next-line react/button-has-type
-      type={htmlType || 'button'}
+      type={type || 'button'}
       className={buttonClass}
       onClick={onClick}
       {...other}
