7735bcbb20c6d24b47464bf644060f586c17be40
Muhammad Naufal <muhammad_naufal@hotmail.com>
2020-11-29 19:17:39 +0700

    Add sessionStorage for homeMachine

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { useMachine } from '@xstate/react';
 import { useHistory } from 'react-router-dom';
+
+import { Movie } from 'data/movie/movie.type';
+import { UserContext } from 'context/UserContext';
+
 import MovieCard from 'components/MovieCard/MovieCard';
 import Spinner from 'components/Spinner/Spinner';
-import { Movie } from 'data/movie/movie.type';
 import Button from 'components/Button/Button';
-import { UserContext } from 'context/UserContext';
 import MoviesGrid from 'components/MoviesGrid/MoviesGrid';
+
 import isMoviePurchased from 'utils/isMoviePurchased';
 import homeMachine from './homeMachine';
+
 import styles from './index.module.scss';
 
 export default function Home() {
-  const [state, send] = useMachine(homeMachine);
   const { userData } = React.useContext(UserContext);
   const history = useHistory();
+  const homeStateSession = 'home-state';
+
+  const persistedState =
+    JSON.parse(sessionStorage.getItem(homeStateSession)!) || homeMachine.initialState;
+  const [state, send, service] = useMachine(homeMachine, {
+    state: persistedState,
+  });
 
   const infiniteScroll = () => {
     const innerHeightPlusScrollTop = window.innerHeight + document.documentElement.scrollTop;
@@ -23,6 +33,15 @@ export default function Home() {
       send({ type: 'FETCH', page: state.context.page + 1 });
     }
   };
+
+  service.onTransition((serviceState) => {
+    try {
+      sessionStorage.setItem(homeStateSession, JSON.stringify(serviceState));
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error('Unable to save to sessionStorage');
+    }
+  });
 
   React.useEffect(() => {
     window.onscroll = () => infiniteScroll();
@@ -52,12 +71,14 @@ export default function Home() {
           </div>
         ))}
       </MoviesGrid>
+
       {state.value === 'failure' && (
         <div className={styles.Feedback}>
           <p>Something went wrong, please try again</p>
           <Button onClick={() => send('RETRY')}>RETRY</Button>
         </div>
       )}
+
       {state.value === 'loading' && (
         <div className={styles.Feedback}>
           <Spinner />
