e940d1efd631070daf37e31cb128d62dd5365c1b
Muhammad Naufal <muhammad_naufal@hotmail.com>
2020-11-29 15:45:47 +0700

    Add route-based code splitting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,26 +1,41 @@
+import Spinner from 'components/Spinner/Spinner';
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Home from './Home';
-import Detail from './Detail';
-import Library from './Library/Library';
+const Home = React.lazy(() => import('./Home'));
+const Detail = React.lazy(() => import('./Detail'));
+const Library = React.lazy(() => import('./Library/Library'));
+
+const SuspenseFallback = (
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <Spinner />
+  </div>
+);
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/:id-:slug">
-        <Detail />
-      </Route>
-      <Route path="/?page=:page">
-        <Home />
-      </Route>
-      <Route path="/library">
-        <Library />
-      </Route>
-      <Route exact path="/">
-        <Home />
-      </Route>
-    </Switch>
+    <React.Suspense fallback={SuspenseFallback}>
+      <Switch>
+        <Route path="/:id-:slug">
+          <Detail />
+        </Route>
+        <Route path="/?page=:page">
+          <Home />
+        </Route>
+        <Route path="/library">
+          <Library />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </React.Suspense>
   );
 };
 
