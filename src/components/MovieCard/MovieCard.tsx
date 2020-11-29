70b93f9e9af8e60549c36712f2ca2fef7ff8c043
Muhammad Naufal <muhammad_naufal@hotmail.com>
2020-11-29 15:46:42 +0700

    Lazy load image in MovieCard component

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazyload';
 
 import { Movie } from 'data/movie/movie.type';
 import formatToCurrency from 'utils/formatToCurrency';
@@ -11,13 +12,17 @@ const MovieCard = ({ movie, isPurchased }: { movie: Movie; isPurchased: boolean 
   const detailUrl = `/${movie.id}-${movie.slug}`;
   return (
     <div key={movie.id} className={styles.MovieCard}>
-      <Link to={detailUrl}>
-        <img
-          className={styles.Poster}
-          src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`}
-          alt={movie.title}
-        />
-      </Link>
+      <LazyLoad once offset={100}>
+        <Link to={detailUrl}>
+          <img
+            className={styles.Poster}
+            src={`https://image.tmdb.org/t/p/w400/${movie.poster_path}`}
+            alt={movie.title}
+            width="640"
+            height="360"
+          />
+        </Link>
+      </LazyLoad>
       <div className={styles.Description}>
         <Link to={detailUrl}>
           <h2 className={styles.Title}>{movie.title}</h2>
